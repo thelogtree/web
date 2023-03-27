f861b13f5fb32bc0c75946864e6267a8cc50d954
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-03-27 12:39:53 -0400

    added ability to copy logs with one click

--- src/screens/Logs/components/Log.tsx
+++ src/screens/Logs/components/Log.tsx
@@ -1,15 +1,19 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Colors } from "src/utils/colors";
 import { StylesType } from "src/utils/styles";
 import { FrontendLog } from "../lib";
 import moment from "moment-timezone";
 import { now } from "lodash";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 type Props = {
   log: FrontendLog;
 };
 
 export const Log = ({ log }: Props) => {
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const [justCopied, setJustCopied] = useState<boolean>(false);
+
   const formattedString = useMemo(() => {
     const now = moment(log.createdAt);
     return (
@@ -19,10 +23,42 @@ export const Log = ({ log }: Props) => {
     );
   }, [log._id]);
 
+  const copyText = useMemo(() => {
+    if (justCopied) {
+      return "📋 copied!";
+    }
+    return isHovering ? "📋 click to copy" : "";
+  }, [justCopied, isHovering]);
+
+  const textToCopy = useMemo(() => {
+    return `${formattedString}\n${log.content}`;
+  }, [log._id]);
+
+  useEffect(() => {
+    if (justCopied) {
+      setTimeout(() => {
+        setJustCopied(false);
+      }, 2000);
+    }
+  }, [justCopied]);
+
   return (
     <div style={styles.container}>
-      <label style={styles.logCreatedAt}>{formattedString}</label>
-      <pre style={styles.pre}>{log.content}</pre>
+      <label style={styles.logCreatedAt}>
+        {formattedString}
+        <span style={styles.copyText}>{copyText}</span>
+      </label>
+      <CopyToClipboard text={textToCopy} onCopy={() => setJustCopied(true)}>
+        <div style={styles.copyBtn}>
+          <pre
+            style={styles.pre}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          >
+            {log.content}
+          </pre>
+        </div>
+      </CopyToClipboard>
     </div>
   );
 };
@@ -40,16 +76,32 @@ const styles: StylesType = {
     backgroundColor: Colors.veryLightGray,
     color: Colors.black,
     borderRadius: 4,
-    padding: 15,
     fontSize: 13,
     width: "100%",
     borderWidth: 1,
     borderColor: Colors.lightGray,
     borderStyle: "solid",
+    padding: 15,
+    textAlign: "left",
   },
   logCreatedAt: {
     color: Colors.gray,
     fontSize: 12,
     paddingBottom: 6,
+  },
+  copyBtn: {
+    outline: "none",
+    border: "none",
+    backgroundColor: Colors.transparent,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    textAlign: "left",
+  },
+  copyText: {
+    paddingLeft: 15,
+    fontWeight: 300,
   },
 };
