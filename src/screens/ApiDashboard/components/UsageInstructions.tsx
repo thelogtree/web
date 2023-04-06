ca4092f30562e79ab667479aba2f1fc867dcdc09
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-04-05 20:21:48 -0400

    Update UsageInstructions.tsx

--- src/screens/ApiDashboard/components/UsageInstructions.tsx
+++ src/screens/ApiDashboard/components/UsageInstructions.tsx
@@ -15,7 +15,7 @@ export const UsageInstructions = () => {
       </label>
       <pre style={styles.preStyle}>
         <div style={styles.codeExample}>
-          {`axios.post("${PROD_SERVER_URL}${serverSlug}/v1/logs",\n  {\n    content: string; // what you want to log\n    folderPath: string; // where you want to log it in logtree. e.g. "/transactions"\n    referenceId?: string; // (optional) refers to an ID in your database (like a userID)\n  },\n  {\n    headers: {\n      "x-logtree-key": "${organization?.keys.publishableApiKey}", // this is your publishable api key\n      "authorization": "your_secret_key"\n    }\n  }\n)`}
+          {`axios.post("${PROD_SERVER_URL}${serverSlug}/v1/logs",\n  {\n    content: string; // what you want to log\n    folderPath: string; // where you want to log it in logtree. e.g. "/transactions"\n    referenceId?: string; // (optional) refers to an ID in your database (like a userID)\n    externalLink?: string; // (optional) provide a website url that you want people to be redirected to if they click on this log.\n  },\n  {\n    headers: {\n      "x-logtree-key": "${organization?.keys.publishableApiKey}", // this is your publishable api key\n      "authorization": "your_secret_key"\n    }\n  }\n)`}
         </div>
       </pre>
       <label style={styles.canUseAnyLanguage}>
