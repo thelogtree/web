2e187de4e612a7922825092820e0a96ec733e017
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-05-08 13:17:05 -0400

    small fix

--- src/screens/Logs/lib.ts
+++ src/screens/Logs/lib.ts
@@ -326,7 +326,7 @@ export const useLogs = (folderId?: string) => {
   }, [isGlobalSearchScreen, isSupportScreen, query]);
 
   return {
-    logs,
+    logs: lastSearchCompletedWithQuery === query ? logs : [],
     numLogsInTotal,
     isLoading,
     attemptFetchingMoreResults,
