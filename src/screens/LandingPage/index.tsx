8344bce59196761ac9c9175e47e6c0e84b5cf133
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-05-31 12:23:27 -0700

    Update index.tsx

--- src/screens/LandingPage/index.tsx
+++ src/screens/LandingPage/index.tsx
@@ -272,7 +272,7 @@ const styles: StylesType = {
     justifyContent: "flex-start",
     alignItems: "center",
     width: "100%",
-    paddingBottom: isMobile ? 150 : 0,
+    paddingBottom: isMobile ? 100 : 0,
   },
   header: {
     display: "flex",
