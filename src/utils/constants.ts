294842f97988096874540da8e2aab6a84b9e8f9d
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-06-12 11:04:19 -0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-  overrideOrgIdForAdmins: "",
+  overrideOrgIdForAdmins: "", // as a logtree admin if I want to view another org's dashboard, then change this to that org's _id. It's preferable to not merge anything other than "" into main though.
 };
