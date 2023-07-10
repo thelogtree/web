4dbf56e6c5ab823e4166429e1af1331dd831dd1c
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2023-07-10 14:37:01 -0700

    Update SetupApp.tsx

--- src/utils/SetupApp.tsx
+++ src/utils/SetupApp.tsx
@@ -99,6 +99,7 @@ export const SetupApp = () => {
     if (authStatus === "SIGNED_IN") {
       fetchUser();
     }
+    console.log(document.referrer)
   }, [authStatus, path]);
 
   useEffect(() => {
